d38353c81afa8c7151d3bb0a922d9d0aa5071f30
xurich-xulaco <82631735+xurich-xulaco@users.noreply.github.com>
2023-10-28 11:16:38 -0600

    Fix typos and pre-configure extentions
    At some point, I thought of doing a per-screenshot suggestions for the settings of each extension, until I dig deeper at dconf and then explored more with gsettings, now it should be easier... Now, we miss some more formatting for the .sh, so that It could be more friendly for anyone.

--- mintfy.sh
+++ mintfy.sh
@@ -1,8 +1,8 @@
 #Installing main themes
-sudo dnf install cinnamon-theme breeze-cursor-theme breeze-icon-theme gnome-shell-theme-yaru papirus-icon-theme
+sudo dnf install cinnamon-themes breeze-cursor-theme breeze-icon-theme gnome-shell-theme-yaru papirus-icon-theme
 
 #disable unnecesary extentions
-gnome-extentions disable blur-my-shell@aunetx
+gnome-extensions disable blur-my-shell@aunetx
 
 #enable convnient extentions
 gnome-extensions enable supergfxctl-gex@asus-linux.org
@@ -21,7 +21,37 @@ sudo dnf install xreader xreader-thumbnailer thunderbird hexchat transmission-gt
 gsettings set org.x.reader.default continuous true
 gsettings set org.x.reader.toolbar show-zoom-action true
 
+gsettings set org.gnome.nautilus.preferences default-sort-order 'type'
+gsettings set org.gtk.Settings.FileChooser sort-directories-first true
+gsettings set org.gtk.gtk4.Settings.FileChooser sort-directories-first true
+
+#Tune-up convinient extentions
 #gnome-extensions prefs arcmenu@arcmenu.com
+gsettings set org.gnome.shell.extensions.arcmenu category-icon-type 'Full_Color'
+gsettings set org.gnome.shell.extensions.arcmenu menu-layout 'Mint'
+gsettings set org.gnome.shell.extensions.arcmenu mint-extra-buttons [['Firefox', 'firefox', 'firefox.desktop'], ['Software', 'org.gnome.Software', 'ArcMenu_Software'], ['Settings', 'org.gnome.Settings', 'org.gnome.Settings.desktop'], ['Terminal', 'org.gnome.Terminal', 'org.gnome.Terminal.desktop'], ['Files', 'org.gnome.Nautilus', 'org.gnome.Nautilus.desktop'], ['Separator', 'list-remove-symbolic', 'ArcMenu_Separator'], ['Lock', 'changes-prevent-symbolic', 'ArcMenu_Lock'], ['Log Out...', 'system-log-out-symbolic', 'ArcMenu_LogOut'], ['Power Off...', 'system-shutdown-symbolic', 'ArcMenu_PowerOff']]
+gsettings set org.gnome.shell.extensions.arcmenu shortcut-icon-type 'Full_Color'
+gsettings set org.gnome.shell.extensions.arcmenu extra-categories [(0, false), (1, false), (2, true), (3, false), (4, true)]
 #gnome-extensions prefs dash-to-panel@jderose9.github.com
+gsettings set org.gnome.shell.extensions.dash-to-panel dot-color-dominant true
+gsettings set org.gnome.shell.extensions.dash-to-panel dot-color-override false
+gsettings set org.gnome.shell.extensions.dash-to-panel dot-style-focused 'SEGMENTED'
+gsettings set org.gnome.shell.extensions.dash-to-panel dot-style-unfocused 'DASHES'
+gsettings set org.gnome.shell.extensions.dash-to-panel focus-highlight-dominant false
+gsettings set org.gnome.shell.extensions.dash-to-panel trans-use-custom-opacity false
+gsettings set org.gnome.shell.extensions.dash-to-panel trans-use-custom-bg true
+gsettings set org.gnome.shell.extensions.dash-to-panel trans-bg-color '#2e2e2e'
 #gnome-extensions prefs gestureImprovements@gestures
+gsettings set org.gnome.shell.extensions.gestureImprovements enable-alttab-gesture false
+gsettings set org.gnome.shell.extensions.gestureImprovements enable-window-manipulation-gesture false
 #gnome-extensions prefs just-perfection-desktop@just-perfection
+gsettings set org.gnome.shell.extensions.just-perfection notification-banner-position 2
+gsettings set org.gnome.shell.extensions.just-perfection osd-position 1
+gsettings set org.gnome.shell.extensions.just-perfection animation 3
+gsettings set org.gnome.shell.extensions.just-perfection activities-button false
+gsettings set org.gnome.shell.extensions.just-perfection startup-status 0
+gsettings set org.gnome.shell.extensions.just-perfection show-apps-button false
+
+#Visit https://github.com/ful1e5/Bibata_Cursor in order to download the latest stable
+
+#Should we use an alternative repo?
